7870902ac655b75c8f5778d3214a2de4f2683358
Ankita1986 <ankitakaswala1@gmail.com>
2020-09-06 00:00:52 +0100

    vending-machine

---
+++ src/main/java/vendingmachine/Coin.java
@@ -0,0 +1,23 @@
+package vendingmachine;
+
+/**
+ * Created by Ankita
+ */
+
+
+//Coins supported by Vending Machine.
+
+public enum Coin {
+    PENNY(1), NICKLE(5), DIME(10), QUARTER(25);
+
+    private int denomination;
+
+    private Coin(int denomination) {
+        this.denomination = denomination;
+    }
+
+    public int getDenomination() {
+        return denomination;
+    }
+}
+
---
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -0,0 +1,20 @@
+package vendingmachine;
+
+import java.util.List;
+
+
+//Declare public API for Vending Machine
+
+public interface VendingMachine {
+
+    public long selectItemAndGetPrice(Item item);
+
+    public void insertCoin(Coin coin);
+
+    public List<Coin> refund();
+
+    public Bucket<Item, List<Coin>> collectItemAndChange();
+
+    public void reset();
+
+}
